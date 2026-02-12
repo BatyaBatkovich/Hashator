e493864bac430ed665a049cb9f8cf24f7dbc46f7
BatyaBatkovich <Daifi.Lucas.Drobchak@protonmail.com>
2026-02-12 17:50:37 +0000

    add modual system

--- hashage.py
+++ hashage.py
@@ -7,95 +7,177 @@ from typing import (
 )
 from Class import Hashator
 
+REGISTRY = {}
+
+
+def register(cls):
+    REGISTRY[cls.__name__] = cls
+    return cls
+
+
+@register
+class sha1:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.sha1(data).hexdigest(), 16)
+
+
+@register
+class md5:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.md5(data).hexdigest(), 16)
+
+
+@register
+class blake2b:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.blake2b(data).hexdigest(), 16)
+
+
+@register
+class blake2s:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.blake2s(data).hexdigest(), 16)
+
+
+@register
+class sha224:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.sha224(data).hexdigest(), 16)
+
+
+@register
+class sha256:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.sha256(data).hexdigest(), 16)
+
+
+@register
+class sha384:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.sha384(data).hexdigest(), 16)
+
+
+@register
+class sha3_224:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.sha3_224(data).hexdigest(), 16)
+
+
+@register
+class sha3_256:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.sha3_256(data).hexdigest(), 16)
+
+
+@register
+class sha3_384:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.sha3_384(data).hexdigest(), 16)
+
+
+@register
+class sha3_512:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.sha3_512(data).hexdigest(), 16)
+
+
+@register
+class sha512:
+    def hash(self, data: bytes) -> int:
+        return int(hashlib.sha512(data).hexdigest(), 16)
+
+
 # source : https://stackoverflow.com/questions/36238076/what-is-the-equivalent-c-c-loselose-algorithm-in-python
-def lose_lose(data: bytes) -> int:
-    hsh = 0
-    for bytes in data:
-        hsh += bytes
-    return hsh
+@register
+class lose_lose:
+    def hash(self, data: bytes) -> int:
+        hsh = 0
+        for byte in data:
+            hsh += byte
+        return hsh
 
 
 # Source : http://www.cse.yorku.ca/~oz/hash.html
-def sdbm(data: bytes) -> int:
-    hsh = 0
-    for byte in data:
-        hsh = byte + (hsh << 6) + (hsh << 16) - hsh
-    return hsh & 0xFFFFFFFF
+@register
+class sdbm:
+    def hash(self, data: bytes) -> int:
+        hsh = 0
+        for byte in data:
+            hsh = byte + (hsh << 6) + (hsh << 16) - hsh
+        return hsh & 0xFFFFFFFF
 
 
-def crc32(data: bytes) -> int:
-    """CRC32 checksum via zlib"""
-    return zlib.crc32(data)
+@register
+class crc32:
+    def hash(self, data: bytes) -> int:
+        return zlib.crc32(data)
 
 
-def djb2(data: bytes) -> int:
-    """DJB2 hash by Dan Bernstein"""
-    hsh = 5381
-    for byte in data:
-        hsh = ((hsh << 5) + hsh) + byte  # hsh * 33 + byte
-    return hsh & 0xFFFFFFFF
+@register
+class djb2:
+    def hash(self, data: bytes) -> int:
+        hsh = 5381
+        for byte in data:
+            hsh = ((hsh << 5) + hsh) + byte  # hsh * 33 + byte
+        return hsh & 0xFFFFFFFF
 
 
-def fletcher16(data: bytes) -> int:
-    sum1 = 0
-    sum2 = 0
-    for byte in data:
-        sum1 = (sum1 + byte) % 255
-        sum2 = (sum2 + sum1) % 255
-    return (sum2 << 8) | sum1
+@register
+class fletcher16:
+    def hash(self, data: bytes) -> int:
+        sum1 = 0
+        sum2 = 0
+        for byte in data:
+            sum1 = (sum1 + byte) % 255
+            sum2 = (sum2 + sum1) % 255
+        return (sum2 << 8) | sum1
 
 
-def custom_hashage(data: bytes) -> int:
-    hsh = 0
-    for byte in data:
-        hsh = hsh + byte
-        hsh = hsh ^ 0x55AA
-        hsh = byte + (hsh << 6) + (hsh << 16) - hsh
-    return hsh
+@register
+class custom_hashage:
+    def hash(self, data: bytes) -> int:
+        hsh = 0
+        for byte in data:
+            hsh = hsh + byte
+            hsh = hsh ^ 0x55AA
+            hsh = byte + (hsh << 6) + (hsh << 16) - hsh
+        return hsh
 
 
 def get_available_algos() -> list[str]:
-    algos: list[str] = []
-
-    try:
-        algos.extend(sorted(hashlib.algorithms_guaranteed))
-    except Exception:
-        pass
-
-    algos.append("lose_lose")
-    algos.append("sdbm")
-    algos.append("djb2")
-    algos.append("crc32")
-    algos.append("fletcher16")
-    algos.append("custom_hashage")
-
-    seen: set[str] = set()
-    unique_algos: list[str] = []
-    for algo in algos:
-        if algo in seen:
-            continue
-        seen.add(algo)
-        unique_algos.append(algo)
-    return unique_algos
+    return REGISTRY.keys()
 
 
 def resolve_hash_algo(name: str) -> Callable[[bytes | Any], Any] | None:
-    custom_hashes = {
-        "lose_lose": lose_lose,
-        "djb2": djb2,
-        "sdbm": sdbm,
-        "crc32": crc32,
-        "fletcher16": fletcher16,
-        "custom_hashage": custom_hashage,
-    }
-
-    if name in custom_hashes:
-        return custom_hashes[name]
+    name_lower = name.lower()
+    if name_lower in REGISTRY:
+        algo_class = REGISTRY[name_lower]
+        instance = algo_class()
+        return instance.hash
 
     try:
         if name in hashlib.algorithms_available:
-            return lambda data, algo=name: hashlib.new(algo, data)
+            return lambda data: int(hashlib.new(name, data).hexdigest(), 16)
     except Exception:
         pass
+
+    return None
+    # custom_hashes = {
+    # "lose_lose": lose_lose,
+    # "djb2": djb2,
+    # "sdbm": sdbm,
+    # "crc32": crc32,
+    # "fletcher16": fletcher16,
+    # "custom_hashage": custom_hashage,
+    # }
+
+    # if name in custom_hashes:
+    # return custom_hashes[name]
+
+    # try:
+    # if name in hashlib.algorithms_available:
+    # return lambda data, algo=name: hashlib.new(algo, data)
+    # except Exception:
+    # pass
 
     return None
